37e6924775bf0f25d81c83bb692450c396b9a0de
kimo <kimokimo191098@gmail.com>
2019-04-29 19:30:46 +0200

    all new

--- 5.py
+++
@@ -1,9 +0,0 @@
-file = open("new","w") 
-print(file)
-file.write("This is first line.") 
-file.write("This is second line.")
-file.close() 
-file = open("new","r") #opening 'new'(filename) in 'read' mode
-t = file.read() # reading file
-print(t)
-file.close()
