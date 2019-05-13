b061f9cc960529bad548c4234b173c5f16b7a912
Aboeleneen <42705383+Aboeleneen@users.noreply.github.com>
2019-05-13 14:53:47 +0200

    update gauss sediel

--- gauss_sediel.py
+++ gauss_sediel.py
@@ -1,55 +1,58 @@
 import numpy as np
 
+class gaussSediel:
 
-def gauss_seidal(A, X_old, b, maxIteration, tor):
-    n = A.shape[0]
-    if not check_convergence(A, n):
-        return False
-    X_new = np.zeros(n, dtype=float)
-    error = np.zeros(n, dtype=float)
-    for k in range(0, maxIteration):
+
+    def __init__(self,A,X_old,b,maxIteration,tor):
+        self.A=A
+        self.X_old=X_old
+        self.maxIteration=maxIteration
+        self.tor=tor
+        self.numEqu=self.A.shape[0]
+        self.error=np.zeros(self.numEqu, dtype=float)
+
+    def solve(self):
+        n = self.numEqu
+        if not self.check_convergence(self.A, n):
+            return False
+        X_new = np.zeros(n, dtype=float)
+        for k in range(0, self.maxIteration):
+            for i in range(0, n):
+                X_new[i] = self.b[i]
+                for j in range(0, n):
+                    if i == j:
+                        continue
+                    if i > j:
+                        X_new[i] = X_new[i] - self.A[i][j] * X_new[j]
+                    else:
+                        X_new[i] = X_new[i] - self.A[i][j] * self.X_old[j]
+                X_new[i] = X_new[i] / self.A[i][i]
+            if self.check_error(X_new, self.X_old, self.tor, n, self.error):
+                return X_new
+            self.X_old[0:] = X_new
+        return X_new
+
+
+    def check_error(X_new, X_old, tor, n, error):
+        done = True
         for i in range(0, n):
-            X_new[i] = b[i]
+            error[i] = (abs(X_new[i] - X_old[i]) / X_new[i]) * 100
+            if error[i] > tor:
+                done = False
+        return done
+
+
+    def check_convergence(A, n):
+        done1 = False
+        done2 = True
+        for i in range(0, n):
+            res = 0
             for j in range(0, n):
                 if i == j:
                     continue
-                if i > j:
-                    X_new[i] = X_new[i] - A[i][j] * X_new[j]
-                else:
-                    X_new[i] = X_new[i] - A[i][j] * X_old[j]
-            X_new[i] = X_new[i] / A[i][i]
-        # print(X_new)
-        # print(X_old)
-        if check_error(X_new, X_old, tor, n, error):
-            return X_new
-        X_old[0:] = X_new
-    return X_new
-
-
-def check_error(X_new, X_old, tor, n, error):
-    done = True
-    for i in range(0, n):
-        error[i] = (abs(X_new[i] - X_old[i]) / X_new[i]) * 100
-        if error[i] > tor:
-            done = False
-    # print(error)
-    # print("\n")
-    return done
-
-
-def check_convergence(A, n):
-    done1 = False
-    done2 = True
-    for i in range(0, n):
-        res = 0
-        for j in range(0, n):
-            if i == j:
-                continue
-            res = res + abs(A[i][j])
-        if res < abs(A[i][i]):
-            done1 = True
-        if res > abs(A[i][i]):
-            done2 = False
-    return (done1 and done2)
-
-
+                res = res + abs(A[i][j])
+            if res < abs(A[i][i]):
+                done1 = True
+            if res > abs(A[i][i]):
+                done2 = False
+        return (done1 and done2)
